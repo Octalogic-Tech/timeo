a3d4efdc8b03dd8ca1386580333a5873979e644b
Vipul Chodankar <vipul72c@gmail.com>
2020-06-30 12:05:13 +0530

    Added Material UI and created Appbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import Navbar from './components/Navbar'
+
+// MUI
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ffffff'
+    },
+    secondary: {
+      main: '#F961A7',
+    },
+  },
+});
 function App() {
+  const [twentyFoHourFormat, setTwentyFoHourFormat] = useState(false);
+  // const [baseTimezone, setBaseTimeZone] = useState();
+  // const [trackedTimezones, setTrackedTimeZones] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={theme}>
+        <Navbar
+          twentyFoHourFormat={twentyFoHourFormat}
+          setTwentyFoHourFormat={setTwentyFoHourFormat}
+        />
+        <Box pt={9}>
+          <Container >
+            <Typography variant="h2">
+              Timeo
+        </Typography>
+          </Container>
+        </Box>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+// MUI
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Switch from '@material-ui/core/Switch';
+import { withStyles } from '@material-ui/core/styles';
+
+const TealSwitch = withStyles({
+  root: {},
+  switchBase: {
+    color: '#34CCB2',
+    '&$checked': {
+      color: '#34CCB2',
+    },
+    '&$checked + $track': {
+      backgroundColor: '#EFEEEE',
+    },
+  },
+  thumb: {},
+  checked: {},
+  track: {
+    backgroundColor: '#EFEEEE',
+  },
+})(Switch);
+
+const Navbar = ({ twentyFoHourFormat, setTwentyFoHourFormat }) => {
+
+  return (
+    <AppBar
+      color="primary"
+    >
+      <Toolbar >
+        <Box
+          display="flex"
+          width={1}
+          alignItems="center"
+          justifyContent="flex-end"
+        >
+          <Typography variant="caption" display="block">
+            12 Hour
+          </Typography>
+          <TealSwitch
+            checked={twentyFoHourFormat}
+            onChange={e => setTwentyFoHourFormat(e.target.checked)}
+            name="Twelve Hour Format"
+
+          />
+          <Typography variant="caption" display="block">
+            24 Hour
+           </Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar
